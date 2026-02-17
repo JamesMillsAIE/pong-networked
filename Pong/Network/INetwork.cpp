4e0467d86ff0dce75ea7300d9e5e1272d9038774
James Mills <jcmills.developer@gmail.com>
2026-02-17 12:18:15 +1100

    Setup the foundation of the network loop

--- Pong/Network/INetwork.cpp
+++ Pong/Network/INetwork.cpp
@@ -4,6 +4,14 @@
 
 #include <Core/Debug.h>
 
+INetwork::INetwork(string hostTypeName)
+	: m_host{ nullptr }, m_address{  }, m_hostTypeName{ std::move(hostTypeName) }
+{
+
+}
+
+INetwork::~INetwork() = default;
+
 int32 INetwork::Initialise()
 {
 	// Attempt to initialise the network system, printing a message if something failed
@@ -26,9 +34,9 @@ int32 INetwork::Initialise()
 
 		// Create a formatted message using the host name type and log it
 		(void)sprintf_s(
-			buffer, 
-			bufferSize, 
-			"An error occurred when while trying to create an ENet %s host.", 
+			buffer,
+			bufferSize,
+			"An error occurred when while trying to create an ENet %s host.",
 			m_hostTypeName.c_str()
 		);
 		Debug::LogLine(buffer);
@@ -54,13 +62,57 @@ void INetwork::Shutdown()
 	enet_deinitialize();
 }
 
-INetwork::INetwork(string hostTypeName)
-	: m_host{ nullptr }, m_address{  }, m_hostTypeName{ std::move(hostTypeName) }
+void INetwork::Tick()
 {
-
+	// Read from the host service, if any events received, handle individually.
+	ENetEvent event;
+	if (enet_host_service(m_host, &event, 0) > 0)
+	{
+		switch (event.type)
+		{
+			// Either a new client host has connected to the server host or when an attempt to establish a
+			// connection with a foreign host has succeeded. Only the "peer" field of the event structure is
+			// valid for this event and contains the newly connected peer.
+			case ENET_EVENT_TYPE_CONNECT:
+			{
+				OnClientConnect(event.peer);
+				break;
+			}
+			// When a connected peer has either explicitly disconnected or timed out. Only the "peer" field of
+			// the event structure is valid for this event and contains the peer that disconnected. Only the "data"
+			// field of the peer is still valid on a disconnect event and must be explicitly reset.
+			case ENET_EVENT_TYPE_DISCONNECT:
+			{
+				OnClientDisconnect(event.peer);
+				break;
+			}
+			// The "peer" field contains the peer the packet was received from, "channelID" is the channel on
+			// which the packet was sent, and "packet" is the packet that was sent. The packet contained in the
+			// "packet" field must be destroyed with enet_packet_destroy() when you are done inspecting its contents.
+			case ENET_EVENT_TYPE_RECEIVE:
+			{
+				OnReceivePacket(event.peer, event.packet);
+				enet_packet_destroy(event.packet);
+				break;
+			}
+			// No event occurred within the specified time limit.
+			case ENET_EVENT_TYPE_NONE:
+			{
+				Debug::LogLine("None event received! This should not be possible!");
+				break;
+			}
+		}
+	}
 }
 
-INetwork::~INetwork() = default;
+void INetwork::OnClientConnect(ENetPeer* peer)
+{}
+
+void INetwork::OnReceivePacket(ENetPeer* peer, ENetPacket* packet)
+{}
+
+void INetwork::OnClientDisconnect(ENetPeer* peer)
+{}
 
 ENetAddress INetwork::CreateAddress()
 {
--- Pong/Network/INetwork.h
+++ Pong/Network/INetwork.h
@@ -6,24 +6,68 @@
 
 #include <string>
 
+/**
+ * A base class for the network system, handles most base functionality, but
+ * is abstract for creating the sided host.
+ */
 class INetwork
 {
 public:
-	int32 Initialise();
-	void Shutdown();
-
-public:
+	/**
+	 * @param hostTypeName This is simply used for logging purposes.
+	 */
 	explicit INetwork(string hostTypeName);
 	virtual ~INetwork();
 
+public:
+	/**
+	 * Attempts to initialise enet and create the relevant variables.
+	 * @return The success state of the initialisation.
+	 */
+	int32 Initialise();
+
+	/** Attempt to shut down enet, destroying the host if possible. */
+	void Shutdown();
+
+	/** Run the network processing loop for a frame. */
+	void Tick();
+
 protected:
+	/**
+	 *
+	 * @param address
+	 * @return
+	 */
 	virtual ENetHost* CreateHost(ENetAddress& address) = 0;
+
+	/**
+	 * 
+	 * @return 
+	 */
 	virtual ENetAddress CreateAddress();
 
-private:
-	ENetHost* m_host;
-	ENetAddress m_address;
+	/**
+	 * 
+	 * @param peer 
+	 */
+	virtual void OnClientConnect(ENetPeer* peer);
 
-	string m_hostTypeName;
+	/**
+	 * 
+	 * @param peer 
+	 * @param packet 
+	 */
+	virtual void OnReceivePacket(ENetPeer* peer, ENetPacket* packet);
+
+	/**
+	 * 
+	 * @param peer 
+	 */
+	virtual void OnClientDisconnect(ENetPeer* peer);
+
+private:
+	ENetHost* m_host; /**< . */
+	ENetAddress m_address; /**< . */
+	string m_hostTypeName; /**< This is simply used for logging purposes. */
 
 };
